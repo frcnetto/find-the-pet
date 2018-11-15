40880d1cde8ed50e86067061bb2b5de1522b69d8
Netto Cavalcante <frcnetto@gmail.com>
2018-11-15 10:11:49 -0300

    Users features
    + Users operations
    + Better errors treatment
    * Config
    
    
    /* info */
    + add
    - remove
    * change

---
+++ dist/common/router.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const events_1 = require("events");
+const restify_errors_1 = require("restify-errors");
+class Router extends events_1.EventEmitter {
+    render(res, next) {
+        return (document) => {
+            if (document) {
+                this.emit('beforeRender', document);
+                res.json(document);
+            }
+            else {
+                throw new restify_errors_1.NotFoundError('Documento não encontrado.');
+            }
+            return next();
+        };
+    }
+}
+exports.Router = Router;
