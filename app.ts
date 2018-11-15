699878e78585d857c5c63a7fe139beb796838fa2
Netto Cavalcante <frcnetto@gmail.com>
2018-11-15 16:20:04 -0300

    Breed features
    + breeds operations
    * breeds and pets routers registered

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import { Server } from './server/server';
 import { usersRouter } from './entities/users/users.router';
 import { locationsRouter } from './entities/locations/locations.router';
+import { petsRouter } from './entities/pets/pets.router';
+import { breedsRouter } from './entities/breeds/breed.router';
 
 const server = new Server();
 
-server.bootstrap( [ usersRouter, locationsRouter ] ).then( server => {
+server.bootstrap( [ usersRouter, locationsRouter, petsRouter, breedsRouter ] ).then( server => {
     console.log( 'Listening on:', server.application.address() );
 } ).catch( error => {
     console.log( 'Server failed to start!' );
